98960f446e5078a853bb7059a8c77a6a1187dc19
a.solonnickov <a.solonnickov@guarantpay.com>
2021-10-18 22:39:03 +0300

    client update: add layout and animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { AuthContext } from "./context";
-import Navbar from "./components/UI/navbar/Navbar";
-import Routes from "./components/Routes";
-import Form from "./components/Form";
-import Footer from "./components/Footer";
+import React from "react";
+import Mainlayout from "./layouts/MainLayout";
 
 export default function App() {
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    if (localStorage.getItem("auth")) {
-      setIsAuth(true);
-    }
-  }, []);
-
   return (
-    <AuthContext.Provider
-      value={{
-        isAuth,
-        setIsAuth,
-      }}
-    >
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Routes />
-          <Form />
-          <Footer/>
-        </div>
-      </BrowserRouter>
-    </AuthContext.Provider>
+    <div>
+      <Mainlayout />
+    </div>
   );
 }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,5 @@
 import AboutPage from "../pages/AboutPage.jsx";
+import Adminpage from "../pages/AdminPage.jsx";
 import StartPage from "../pages/StartPage.jsx";
 import Todopage from "../pages/ToDoPage.jsx";
 
@@ -6,5 +7,6 @@ export const privateRoutes = [
   { path: "/", component: StartPage, exact: true },
   { path: "/about", component: AboutPage, exact: true },
   { path: "/todo", component: Todopage, exact: true },
+  { path: "/admin", component: Adminpage, exact: true },
 ];
 export const publicRoutes = [{ path: "/", component: StartPage, exact: true }];
