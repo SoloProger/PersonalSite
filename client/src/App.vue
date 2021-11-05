32d5170ba81924a88551cd476bac9cd0971edd3d
a.solonnickov <a.solonnickov@guarantpay.com>
2021-11-05 08:32:14 +0300

    Create new client branch with Vue

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+
+    <router-view/>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
