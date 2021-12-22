b9d0ad04e037c13d64bd27638f23286867a31ada
SoloProger <solo.proger@gmail.com>
2021-12-22 10:39:47 +0300

    Add destroy endpoint

--- server/app/Http/Controllers/TodoController.php
+++ server/app/Http/Controllers/TodoController.php
@@ -85,8 +85,10 @@ class TodoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(ToDo $todo)
     {
-
+        $todo->delete();
+        $message = "Success delete todo!";
+        return $message;
     }
 }
