3d63bfbf516ed3b1c3a79097827fe5ce4cb5e3c7
SoloProger <solo.proger@gmail.com>
2022-04-14 00:06:39 +0300

    [Server-3] Migrate on Nest.js

---
+++ server/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const PORT = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Документация API для персонального сайта')
+    .setDescription(
+      'Подробная документация для персонального сайта, сгенерированная с помощью Swagger',
+    )
+    .setVersion('1.0.0')
+    .addTag('Personal Site')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  await app.listen(PORT, () =>
+    console.log(`Server started. http://localhost:${PORT}`),
+  );
+}
+bootstrap();
